71a187545ae3385640fb3a5855f31892354d4476
oweeeenie <owensullivan419@yahoo.com>
2024-12-06 03:55:16 -0500

    added basic append and prepend, also returning the head, tail, and size.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,42 @@ class LinkedLists {
     this.size = 0;
   }
 
-  append(value) {}
+  append(value) {
+    let nodeEnd = new Node(value);
+
+    if (this.head === null) {
+      this.head = nodeEnd;
+      this.tail = nodeEnd;
+    } else {
+      this.tail.next = nodeEnd;
+      this.tail = nodeEnd;
+    }
+    this.size++;
+  }
+
+  prepend(value) {
+    let nodeStart = new Node(value);
+    if (this.head === null) {
+      this.head = nodeStart;
+      this.tail = nodeStart;
+    } else {
+      this.head.next = nodeStart;
+      this.head = nodeStart;
+    }
+    this.size++;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    return this.tail;
+  }
+
+  getSize() {
+    return this.size;
+  }
 }
 
 class Node {
