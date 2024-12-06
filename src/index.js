fd0560eb13ffb374f21b1eb63fb592910bca30d4
oweeeenie <owensullivan419@yahoo.com>
2024-12-05 21:35:38 -0500

    added a very basic beginning. had no time to do much.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './styles.css';
+
+class LinkedLists {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value) {}
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
